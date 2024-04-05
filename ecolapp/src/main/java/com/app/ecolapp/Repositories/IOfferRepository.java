e578ea79974357a5e23e618cbde954f49197b51e
Juan Jose Alvarez Beltran <143049108+JuanalvarezECI@users.noreply.github.com>
2024-04-05 16:18:31 -0500

    Delete ecolapp directory

--- ecolapp/src/main/java/com/app/ecolapp/Repositories/IOfferRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.ecolapp.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.ecolapp.Models.OfferModel;
-
-public interface IOfferRepository extends JpaRepository<OfferModel, Long>{
-
-}
